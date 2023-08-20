1917e00c253b5aea8dec04ae7cd812d537c4323b
xxai.art <xxai.art@gmail.com>
2023-08-21 07:48:27 +0800

    ♨

---
+++ ol/up.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+
+cd $(hostname)
+direnv allow
+name=xxai.art
+
+mkdir_ln() {
+  if [ ! -d "$1" ]; then
+    fp=/$2/$1/$name
+    mkdir -p $fp
+    ln -s $fp $1
+  fi
+
+}
+
+mkdir_ln log var
+mkdir_ln cache mnt
+mkdir_ln data mnt
+
+direnv exec . docker-compose up -d
