f731caba30568e99541fae3ef87c93b7850255af
xxai.art <xxai.art@gmail.com>
2023-08-24 18:49:40 +0800

    ♨

--- ol/down.sh
+++ ol/down.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -ex
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 DIR=$(realpath $0) && DIR=${DIR%/*}
+set -ex
 cd $DIR/$(hostname)
 direnv allow
 direnv exec . docker-compose down
